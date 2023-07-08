0352be43ac8942eba637079cbbbd2744cbd19fe9
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2023-07-08 11:13:54 +0600

    ./

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     {{-- <div class="container">
         <div class="cursor"></div>
       </div> --}}
-    <div class=" text-white shadow-lg container" id="main">
+    <div class=" text-white shadow-lg rounded-3xl mt-2 container h-full" id="main">
 
         <div class="grid grid-cols-5 gap-1 text-2xl text-slate-950 font-mono" id="nav">
             <div class=" p-1 col-span-1" id="navLogo">logo</div>
@@ -57,8 +57,7 @@
         {{-- Layout A --}}
         <div class=" m-1 p-1 items-center " id="layoutA">
             <div class=" grid grid-cols-1" id="layoutAA">
-                <div class="text-center h-72 flex justify-center items-center"
-                    id="layoutAAA">
+                <div class="text-center h-72 flex justify-center items-center" id="layoutAAA">
                     <div class="">
                         <h1 class="text-5xl p-1 typing-effect text-">Welcome to Report Gov Bangladesh</h1>
                         <p class="text-3xl">The Idea is to help the people
@@ -131,6 +130,104 @@
                 What you Can do with ReportGovBD
             </p>
         </div>
+        <!-- component -->
+
+            <section class="text-gray-600 body-font">
+                <div class="">
+                    <div class="flex flex-col text-center w-full ">
+                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac
+                            Heirloom</h1>
+                        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon
+                            brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't
+                            heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac
+                            humblebrag.</p>
+                    </div>
+                    <div class="flex flex-wrap  text-center">
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                                    viewBox="0 0 24 24">
+                                    <path d="M8 17l4 4 4-4m-4-5v9"></path>
+                                    <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+                                </svg>
+                                <h2 class="title-font font-medium text-3xl text-gray-900">2.7K</h2>
+                                <p class="leading-relaxed">Downloads</p>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                                    viewBox="0 0 24 24">
+                                    <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+                                    <circle cx="9" cy="7" r="4"></circle>
+                                    <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+                                </svg>
+                                <h2 class="title-font font-medium text-3xl text-gray-900">1.3K</h2>
+                                <p class="leading-relaxed">Users</p>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round"
+                                    stroke-linejoin="round" stroke-width="2"
+                                    class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                                    <path d="M3 18v-6a9 9 0 0118 0v6"></path>
+                                    <path
+                                        d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z">
+                                    </path>
+                                </svg>
+                                <h2 class="title-font font-medium text-3xl text-gray-900">74</h2>
+                                <p class="leading-relaxed">Files</p>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round"
+                                    stroke-linejoin="round" stroke-width="2"
+                                    class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                                </svg>
+                                <h2 class="title-font font-medium text-3xl text-gray-900">46</h2>
+                                <p class="leading-relaxed">Places</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RGBD</span>
+                    </a>
+                    <ul
+                        class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                        </li>
+                        <li>
+                            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
+                        href="https://flowbite.com/" class="hover:underline">RGBD</a>. All Rights Reserved.</span>
+            </div>
+        </footer>
+
 
     </div>
 
